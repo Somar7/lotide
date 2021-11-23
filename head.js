a3fcb756af9d582b7909b40cb98a64a62a85a92f
somar7 <somar7@nait.ca>
2021-11-23 02:43:23 +0000

    edited test code

--- head.js
+++ head.js
@@ -3,11 +3,20 @@ const head = function(array) {
   return array[0]
 };
 
-const assertEqual = require('./assertEqual');
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
+
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+
+  }
+
+};
 
 // TEST CODE
 assertEqual(head([5,6,7]), 5); //PASS
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Helloo"); //FAIL
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); //FAIL
 
 
-module.exports = head;
