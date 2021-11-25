acb92042a924b57e114b01e3ac787db6cdda1c75
somar7 <somar7@nait.ca>
2021-11-25 22:29:11 +0000

    Assert Arrays Eq

---
+++ assertArraysEqual.js
@@ -0,0 +1,35 @@
+const eqArrays = function(arr1, arr2) {
+
+  let i = 0;
+  let answer = Boolean;
+
+  for (i = 0; i < arr1.length; i++) {
+
+    if (arr1.length === arr2.length) {
+      if (arr1[i] === arr2[i]) {
+        answer = true;
+      } else if (arr1[i] !== arr2[i]) {
+        answer = false;
+      }
+    } else {
+      answer = false
+    }
+  }
+  return answer;
+
+};
+
+let assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays (arr1, arr2)) {
+      console.log('pass');
+  } else {
+      console.log('fail');
+  }
+};
+
+module.exports = assertArraysEqual; 
+
+assertArraysEqual((eqArrays)([1, 2, 3], [3, 2, 1])); // => false
+assertArraysEqual((eqArrays)([1, 2, 3], [1, 2, 3])); // => true
+assertArraysEqual((eqArrays)(["1", "2", "3"], ["1", "2", "3"])); // => true
+assertArraysEqual((eqArrays)(["1", "2", "3"], ["1", "2", 3])); // => false
